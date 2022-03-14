c3e425a50ab0cd42343a4823bb0b464d5e916f9f
github daisukenoguchi <daisukenoguemon@gmail.com>
2022-03-14 20:27:25 +0900

    divide go-reddit

--- pkg/domain/post.go
+++ pkg/domain/post.go
@@ -1,65 +1,12 @@
 package domain
 
 import (
-	"context"
-	"errors"
-	"fmt"
-	"github.com/caarlos0/go-reddit/v3/reddit"
+	go_reddit "github.com/caarlos0/go-reddit/v3/reddit"
 )
-
-var(
-	ErrorFailedToFetchPosts = "failed to fetch posts"
-	ErrorSubredditNotExist = "subreddit does not exist"
-)
-
-const defaultPostsLimit = 10
-
-type Option struct {
-	Limit int
-}
 
 type Post struct {
-	*reddit.Post
+	*go_reddit.Post
 	body_ja string
 	title_ja string
 }
 
-func GetPostList(subreddit string, option Option) ([]*Post, error) {
-	if subreddit == "" {
-		return nil, errors.New(ErrorSubredditNotExist)
-	}
-
-	if option.Limit == 0 {
-		option.Limit = defaultPostsLimit
-	}
-
-	rawPosts, _, err := reddit.DefaultClient().Subreddit.NewPosts(context.Background(), subreddit, &reddit.ListOptions{
-		Limit: option.Limit,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("error message: %v, received values: %v", err, rawPosts)
-	}
-
-	posts := make([]*Post, len(rawPosts))
-	for i, post := range rawPosts {
-		posts[i] = &Post{
-			Post: post,
-		}
-	}
-
-	return posts, nil
-}
-
-func GetPostByID(postID string) (*reddit.PostAndComments, error) {
-	postComments, _, err := reddit.DefaultClient().Post.Get(context.Background(), postID)
-	if err != nil {
-		return nil, err
-	}
-
-	comments := postComments.Comments
-	if len(comments) > 10 {
-		postComments.Comments = comments[:10]
-	}
-
-	return postComments, nil
-}
--- pkg/infra/reddit/reddit.go
+++ pkg/infra/reddit/reddit.go
@@ -1,1 +1,51 @@
 package reddit
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	go_reddit "github.com/caarlos0/go-reddit/v3/reddit"
+)
+
+var (
+	ErrorFailedToFetchPosts = "failed to fetch posts"
+	ErrorSubredditNotExist  = "subreddit does not exist"
+)
+
+const defaultPostsLimit = 10
+
+type Option struct {
+	Limit int
+}
+
+func FetchPostList(subreddit string, option Option) ([]*go_reddit.Post, error) {
+	if subreddit == "" {
+		return nil, errors.New(ErrorSubredditNotExist)
+	}
+
+	if option.Limit == 0 {
+		option.Limit = defaultPostsLimit
+	}
+	posts, _, err := go_reddit.DefaultClient().Subreddit.NewPosts(context.Background(), subreddit, &go_reddit.ListOptions{
+		Limit: option.Limit,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error message: %v, received values: %v", err, posts)
+	}
+
+	return posts, nil
+}
+
+func FetchPostAndCommentsByID(postID string) (*go_reddit.PostAndComments, error) {
+	postComments, _, err := go_reddit.DefaultClient().Post.Get(context.Background(), postID)
+	if err != nil {
+		return nil, err
+	}
+
+	comments := postComments.Comments
+	if len(comments) > 10 {
+		postComments.Comments = comments[:10]
+	}
+
+	return postComments, nil
+}
